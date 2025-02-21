2cf3889daa7723b153e0843a63d0db088698b12a
SudhanshuDandriyal <95971555+SudhanshuDandriyal@users.noreply.github.com>
2025-02-21 15:18:51 +0530

    Updated Flask app to run on port 5000

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def upload_file():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
